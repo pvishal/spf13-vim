728bdc357561a93ce6ca22e1b249f0263cf41146
Vishal Prabhu <vishal.prabhu@gmail.com>
2012-04-15 21:32:56 +0530

    Modified the install script to work with paths having spaces

--- spf13-vim-windows-install.cmd
+++ spf13-vim-windows-install.cmd
@@ -4,9 +4,9 @@
 :: Set the current directory as the base directory. Make appropriate links
 @set BASE_DIR=%CD%
 :: call git clone --recursive -b 3.0 git://github.com/spf13/spf13-vim.git %BASE_DIR%
-call mkdir %BASE_DIR%\.vim\bundle
-call mklink /J %HOME%\.vim %BASE_DIR%\.vim
-call mklink %HOME%\.vimrc %BASE_DIR%\.vimrc
-call mklink %HOME%\_vimrc %BASE_DIR%\.vimrc
+call mkdir "%BASE_DIR%\.vim\bundle"
+call mklink /J "%HOME%\.vim" "%BASE_DIR%\.vim"
+call mklink "%HOME%\.vimrc" "%BASE_DIR%\.vimrc"
+call mklink "%HOME%\_vimrc" "%BASE_DIR%\.vimrc"
 
-call git clone http://github.com/gmarik/vundle.git %HOME%/.vim/bundle/vundle
+call git clone http://github.com/gmarik/vundle.git "%HOME%/.vim/bundle/vundle"
